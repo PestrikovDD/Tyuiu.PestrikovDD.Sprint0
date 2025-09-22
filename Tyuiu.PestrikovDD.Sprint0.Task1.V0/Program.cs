ec18a826f91254e7c5917f3e2689742f8ea04e76
Данила Пестриков <Tyuiu.PestrikovDD1@outlook.com>
2025-09-22 14:19:46 +0500

    Task1

---
+++ Tyuiu.PestrikovDD.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.PestrikovDD.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Данила";
+            int age = 22;
+            Console.WriteLine("Привет меня зовут " + name + " мне " + age + " лет! " );
+            Console.ReadKey();
+        }
+    }
+}
